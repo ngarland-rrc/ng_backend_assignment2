d63fbb87a0f0aa862323e7258d0143ee3358d6d3
Nick Garland <ngarland@academic.rrc.ca>
2026-02-04 16:42:27 -0600

    Feat: added api folder with controllers,models,routes and services folder as well as added the ticketRoutes file

---
+++ src/api/v1/routes/ticketRoutes.ts
@@ -0,0 +1,12 @@
+import express, { Router } from "express";
+import * as ticketController from "../controllers/ticketController";
+
+const router: Router = express.Router();
+
+router.get("/", ticketController.getAllTickets);
+router.get("/:id/urgency", ticketController.getTicketUrgencyScore);
+router.post("/", ticketController.createTicket);
+router.put("/:id", ticketController.updateTicket);
+router.delete("/:id", ticketController.deleteTicket);
+
+export default router;
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express";
 import morgan from "morgan";
+import ticketRoutes from "./api/v1/routes/ticketRoutes";
 
 const app: Express = express();
 
@@ -14,6 +15,10 @@ app.use(morgan("combined"));
 
 app.use(express.json());
 
+/**
+ * Health check endpoint that returns server status information
+ * @returns JSON response with server health metrics
+ */
 app.get("/api/v1/health", (req, res) => {
     const healthData: HealthCheckResponse = {
         status: "OK",
@@ -24,5 +29,7 @@ app.get("/api/v1/health", (req, res) => {
 
     res.json(healthData);
 });
+
+app.use("/api/v1/tickets", ticketRoutes)
 
 export default app;
